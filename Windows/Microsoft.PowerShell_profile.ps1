b670c5052a33de941a816e8471d02dd4512e30a0
Ethan Tran <rizcielbleu@gmail.com>
2021-02-18 17:31:56 -0500

    Update oh-my-posh

--- Windows/Microsoft.PowerShell_profile.ps1
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -1,28 +1,2 @@
-[ScriptBlock]$Prompt = {
-    $realLASTEXITCODE = $global:LASTEXITCODE
-    if ($realLASTEXITCODE -isnot [int]) {
-      $realLASTEXITCODE = 0
-    }
-    $startInfo = New-Object System.Diagnostics.ProcessStartInfo
-    $startInfo.FileName = "C:\Users\rizci\.ohmyposh\oh-my-posh.exe"
-    $cleanPWD = $PWD.ProviderPath.TrimEnd("\")
-    $startInfo.Arguments = "-config=""C:\Users\rizci\.ohmyposh\poshthemes\paradox.json"" -error=$realLASTEXITCODE -pwd=""$cleanPWD"""
-    $startInfo.Environment["TERM"] = "xterm-256color"
-    $startInfo.CreateNoWindow = $true
-    $startInfo.StandardOutputEncoding = [System.Text.Encoding]::UTF8
-    $startInfo.RedirectStandardOutput = $true
-    $startInfo.UseShellExecute = $false
-    if ($PWD.Provider.Name -eq 'FileSystem') {
-      $startInfo.WorkingDirectory = $PWD.ProviderPath
-    }
-    $process = New-Object System.Diagnostics.Process
-    $process.StartInfo = $startInfo
-    $process.Start() | Out-Null
-    $standardOut = $process.StandardOutput.ReadToEnd()
-    $process.WaitForExit()
-    $standardOut
-    $global:LASTEXITCODE = $realLASTEXITCODE
-    Remove-Variable realLASTEXITCODE -Confirm:$false
-}
-Set-Item -Path Function:prompt -Value $Prompt -Force
+Set-PoshPrompt -Theme robbyrussel.omp
 New-Alias g git
