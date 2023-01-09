f5c700a418e62db17d60a020c823c25b5060fd75
Ethan Tran <rizcielbleu@gmail.com>
2023-01-09 12:40:24 -0500

    Wallpapers per monitor

--- .config/scripts/lock.py
+++ .config/scripts/lock.py
@@ -6,8 +6,15 @@ import subprocess
 FOLDER_PATH = os.path.join("/home/entran/files/wallpapers", socket.gethostname())
 
 def main():
+    if (socket.gethostname() == "cakebatter"):
+        left_path = os.path.join(FOLDER_PATH, "left_monitor")
+        right_path = os.path.join(FOLDER_PATH, "right_monitor")
+        left_wallpaper = random.choice([os.path.join(left_path, file) for file in os.listdir(left_path)])
+        right_wallpaper = random.choice([os.path.join(right_path, file) for file in os.listdir(right_path)])
+        subprocess.call(["swaylock", "--clock", "--indicator","-i", f"DP-3:{left_wallpaper}", "-i", f"DP-4:{right_wallpaper}"])
+    elif (socket.gethostname() == "mintchip"):
         wallpaper = random.choice([os.path.join(FOLDER_PATH, file) for file in os.listdir(FOLDER_PATH)])
-        subprocess.call(["swaylock", "--clock", "--indicator", "-i", wallpaper])
+        subprocess.call(["swaylock", "--clock", "--indicator","-i", wallpaper])
 
 if __name__ == "__main__":
     main()
--- .config/scripts/wallpaper_change.py
+++ .config/scripts/wallpaper_change.py
@@ -11,21 +11,41 @@ TIMER_DELAY = 0.75
 def change_wallpaper(wallpaper: str):
     subprocess.call(["swww", "img", wallpaper, "--transition-duration", "1", "--transition-type", "random"])
 
+def change_wallpaper(left_wallpaper: str, right_wallpaper: str):
+    subprocess.call(["swww", "img", "-o", "DP-3", left_wallpaper, "--transition-duration", "1", "--transition-type", "random"])
+    subprocess.call(["swww", "img", "-o", "DP-4", right_wallpaper, "--transition-duration", "1", "--transition-type", "random"])
+
 def main():
     time.sleep(1)
-    wallpapers = itertools.cycle([os.path.join(FOLDER_PATH, file) for file in os.listdir(FOLDER_PATH)])
-    change_wallpaper(next(wallpapers))
+    if (socket.gethostname() == "cakebatter"):
+        left_path = os.path.join(FOLDER_PATH, "left_monitor")
+        right_path = os.path.join(FOLDER_PATH, "right_monitor")
+        left_wallpapers = itertools.cycle([os.path.join(left_path, file) for file in os.listdir(left_path)])
+        right_wallpapers = itertools.cycle([os.path.join(right_path, file) for file in os.listdir(right_path)])
+        change_wallpaper(next(left_wallpapers), next(right_wallpapers))
+    elif (socket.gethostname() == "mintchip"):
+        wallpapers = itertools.cycle([os.path.join(FOLDER_PATH, file) for file in os.listdir(FOLDER_PATH)])
+        change_wallpaper(next(wallpapers))
     with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
         signature = os.getenv("HYPRLAND_INSTANCE_SIGNATURE")
         client.connect(f"/tmp/hypr/{signature}/.socket2.sock")
         fd = client.makefile()
-        timer = threading.Timer(TIMER_DELAY, change_wallpaper, next(wallpapers))
-        while True:
-            line = fd.readline()
-            if line[0:9] == "workspace":
-                timer.cancel()
-                timer = threading.Timer(TIMER_DELAY, change_wallpaper, [next(wallpapers)])
-                timer.start()
+        if (socket.gethostname() == "cakebatter"):
+            timer = threading.Timer(TIMER_DELAY, change_wallpaper, [next(left_wallpapers), next(right_wallpapers)])
+            while True:
+                line = fd.readline()
+                if line[0:9] == "workspace":
+                    timer.cancel()
+                    timer = threading.Timer(TIMER_DELAY, change_wallpaper, [next(left_wallpapers), next(right_wallpapers)])
+                    timer.start()
+        elif (socket.gethostname() == "mintchip"):
+            timer = threading.Timer(TIMER_DELAY, change_wallpaper, next(wallpapers))
+            while True:
+                line = fd.readline()
+                if line[0:9] == "workspace":
+                    timer.cancel()
+                    timer = threading.Timer(TIMER_DELAY, change_wallpaper, [next(wallpapers)])
+                    timer.start()
 
 if __name__ == "__main__":
     main()
