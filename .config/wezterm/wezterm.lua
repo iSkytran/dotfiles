eaecf1de2f2ad8e4080d6b46352734dbd82f1c1b
Ethan Nhu Tran <rizcielbleu@gmail.com>
2024-03-24 18:37:16 -0400

    Wezterm hide tabbar when one tab

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Catppuccin Mocha'
 config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font('JetBrainsMono Nerd Font')
 
 config.keys = {
