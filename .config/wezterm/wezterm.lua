560fb5eead9843d9fc021fa6f4b4423334c969e3
Ethan Nhu Tran <rizcielbleu@gmail.com>
2024-04-08 10:54:22 -0400

    Wezterm: Update font size and change leader

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,11 +6,10 @@ config.color_scheme = 'Catppuccin Mocha'
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font('JetBrainsMono Nerd Font')
-config.font_size = 10.0
 
 config.keys = {
   {
-    key = 'Space',
+    key = 'a',
     mods = 'CTRL',
     action = wezterm.action.ActivateKeyTable {
       name = 'table_mode',
